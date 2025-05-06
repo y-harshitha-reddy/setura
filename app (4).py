2be092b5c6d9d1dd11d6dcd923278fabbd885641
y-harshitha-reddy <yeddulaharshitha2005@gmail.com>
2025-05-06 15:47:00 +0530

    Update app (4).py

--- app (4).py
+++ app (4).py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
+pip install serpapi
 from serpapi import GoogleSearch
 import plotly.express as px
 import re
